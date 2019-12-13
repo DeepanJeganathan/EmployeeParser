b67ad8dfda95ac23f61ec6e4bd42f677dab1772c
deepan2223jega@gmail.com <deepan2223jega@gmail.com>
2019-12-13 04:39:30 -0500

    Initial

---
+++ employeeParser/Parser.cs
@@ -0,0 +1,120 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace employeeParser
+{
+    public class Parser
+    {
+        private string _filePath;
+        private Dictionary<int, List<object>> _fullList;
+        private int _startRow;
+        private int _lastRow;
+        private List<int> _columnNum;
+        private List<string> _txt;
+        public Parser(string filePath)
+        {
+            this._filePath = filePath;
+        }
+
+        public void CreateExcelPackage(string WorksheetName, List<int> ColumnNum, int startRow, int lastRow)
+        {
+            FileInfo fi = new FileInfo(_filePath);
+            using (ExcelPackage excelPackage = new ExcelPackage(fi))
+            {
+                ExcelWorksheet Worksheet = excelPackage.Workbook.Worksheets[WorksheetName];
+
+                var fullList = new Dictionary<int, List<object>>();
+                foreach (var column in ColumnNum)
+                {
+                    
+                    List<object> columnList = new List<object>();
+
+                    for (int i = startRow; i < lastRow; i++)
+                    {
+                        columnList.Add(Worksheet.Cells[i,column].Value);
+                    }
+
+                    fullList.Add(column, columnList);
+
+                   
+                }
+
+                this._fullList = fullList;
+                this._startRow = startRow;
+                this._lastRow = lastRow;
+                this._columnNum = ColumnNum;
+            }
+          
+
+        }
+
+        public Dictionary<int,List<object>> GetDict()
+        {
+            return _fullList;
+
+        }
+
+        public void GetFormattedList()
+        {
+            var ans = _fullList;
+
+            List<List<object>> ansList = new List<List<object>>();
+
+            foreach (var item in ans)
+            {
+                ansList.Add(item.Value);
+            }
+
+            List<string> txt = new List<string>();
+
+            for (int i = 0; i < (_lastRow - _startRow); i++)
+            {
+                var stringAns = "";
+                for (int k = 0; k < _columnNum.Count; k++)
+                {
+                   
+                    stringAns+= ansList[k][i];
+                }
+                txt.Add(stringAns);
+
+                Console.WriteLine(stringAns);
+
+                stringAns = "";
+
+            }
+
+            foreach (var item in txt)
+            {
+                Console.WriteLine(item);
+            }
+
+            this._txt = txt;
+
+
+        }
+
+        public void ExportToTxtFile(string TextFilePath)
+        {
+            GetFormattedList();
+            foreach (var item in _txt)
+            {
+                using (System.IO.StreamWriter file =
+                                                new System.IO.StreamWriter(TextFilePath, true))
+                {
+                    file.WriteLine(item);
+                }
+
+            }
+
+        }
+
+
+    }
+}
+
+ 
---
+++ employeeParser/Program.cs
@@ -0,0 +1,125 @@
+﻿
+using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace employeeParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<int> nums = new List<int>() { 2, 3, 4, 6, 9 };
+
+            Parser p = new Parser(@"C:\Users\14168\Desktop\EmployeeMatrix.xlsx");
+            p.CreateExcelPackage("Two",nums,2,56);
+
+
+            var ans=p.GetDict();
+            p.GetFormattedList();
+            p.ExportToTxtFile(@"C:\Users\14168\Desktop\EmployeeMatrix.txt");
+           
+            Console.ReadKey();
+
+
+
+
+        }
+
+
+        //        string filePath = @"C:\Users\14168\Desktop\EmployeeMatrix.xlsx";
+
+
+
+        //        FileInfo fi = new FileInfo(filePath);
+        //            using (ExcelPackage excelPackage = new ExcelPackage(fi))
+        //            {
+        //                ExcelWorksheet firstWorksheet = excelPackage.Workbook.Worksheets["Two"];
+
+        //    List<object> columnB = new List<object>();
+
+        //                for (int i = 2; i<56; i++)
+        //                {
+        //                   columnB.Add(firstWorksheet.Cells[i, 2].Value);
+        //                }
+
+
+        //List<object> columnC = new List<object>();
+
+        //                for (int i = 2; i< 56; i++)
+        //                {
+        //                    columnC.Add(firstWorksheet.Cells[i, 3].Value);
+        //                }
+
+        //                List<object> columnD = new List<object>();
+
+        //                for (int i = 2; i< 56; i++)
+        //                {
+        //                    columnD.Add(firstWorksheet.Cells[i, 4].Value);
+        //                }
+
+        //                List<object> columnF = new List<object>();
+
+        //                for (int i = 2; i< 56; i++)
+        //                {
+        //                    columnF.Add(firstWorksheet.Cells[i, 6].Value);
+        //                }
+
+        //                List<object> columnI = new List<object>();
+
+        //                for (int i = 2; i< 56; i++)
+        //                {
+        //                    columnI.Add(firstWorksheet.Cells[i, 9].Value);
+        //                }
+
+        //                List<int> nullValues = new List<int>();
+
+        //int k = 0;
+        //                foreach (var item in columnB)
+        //                {
+        //                    k++;
+        //                    if (item == null)
+        //                    {
+        //                        nullValues.Add(k-1);
+        //                    }
+        //                }
+
+        //                for (int i = 0; i< 50; i++)
+        //                {
+        //                    if (!((nullValues[0] == i) || (nullValues[1] == i) || (nullValues[2] == i)))
+        //                    {
+
+        //                        using (System.IO.StreamWriter file =
+        //                         new System.IO.StreamWriter(@"C:\Users\14168\Desktop\EmployeeMatrix.txt", true))
+        //                        {
+        //                            file.WriteLine(columnB[i].ToString()+ columnC[i].ToString() + DateTime.Parse(columnD[i].ToString()).Year+ columnF[i].ToString()+ columnI[i].ToString());
+        //                        }
+        //                        Console.WriteLine(" operator #: {0}, operator name: {1}, Hire date:{2},PayGrade:{3}, primary:{4}", columnB[i], columnC[i], DateTime.Parse(columnD[i].ToString()).Year, columnF[i], columnI[i]);
+
+        //                    }
+
+        //                }
+
+
+
+        //                }
+
+
+        //                Console.ReadKey();
+
+
+
+
+        //            }
+
+
+
+    }
+
+    }
+
